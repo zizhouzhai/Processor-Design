270f923c341392eacfd28acb38e8184aa194def4
yupei <yupei@ucsd.edu>
2015-02-05 15:34:40 -0800

    added branch forward and backward input
    needed forward and backward branch rather than just branch for the input

--- pc.v
+++ pc.v
@@ -1,14 +1,16 @@
 module pc(
 	input start_i,
 	input [7:0] startadd_i,
-	input branch_i,
+	input branchb_i,
+	input branchf_i,
 	input [7:0] target_i,  //PC-relative address not absolute
 	output [7:0] pc_o);
 
 	reg [7:0] prgcount;
 
 	assign  pc_o = (start_i) ? startadd_i : 
-			  (branch_i) ? (pc_o + 1 + target_i):
+			  (branchf_i) ? (pc_o + 1 + target_i):
+			  (branchb_i) ? (pc_o + 1 - target_i):
 			  (pc_o + 1);
 
 endmodule
