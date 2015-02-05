a94d570c215daf02c1fd3706a939882fdec7d29a
Longfei Li <codyli520@gmail.com>
2015-02-05 15:07:18 -0800

    instROM correction

--- instROM_test.v
+++ instROM_test.v
@@ -13,31 +13,30 @@ initial
 	address_i = 7'b0000001;
 	#5
 		$display("Current address is %h,instruction is %d",address_i, data_o);
-	#5
 	address_i = 7'b0000010;
-		$display("Current address is %h,instruction is %d",address_i, data_o);
 	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 	address_i = 7'b0000011;
-		$display("Current address is %h,instruction is %d",address_i, data_o);
 	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 	address_i = 7'b0000100;
+	#5
 		$display("Current address is %h,instruction is %d",address_i, data_o);
-	#5
 	address_i = 7'b0000101;
-		$display("Current instruction is %d", data_o);
 	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 	address_i = 7'b0000110;
-		$display("Current instruction is %d", data_o);
 	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 	address_i = 7'b0000111;
-		$display("Current instruction is %d", data_o);
 	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 	address_i = 7'b0001000;
-		$display("Current instruction is %d", data_o);
 	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 	address_i = 7'b0001001;
-		$display("Current instruction is %d", data_o);
-	
+	#5
+		$display("Current address is %h,instruction is %d",address_i, data_o);
 
 	end
 endmodule
