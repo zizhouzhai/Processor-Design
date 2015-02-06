ac943a55a45a473029b440ccf349b0dd344b8263
zizhouzhai <zizhou.zhai@hotmail.co.nz>
2015-02-05 18:12:53 -0800

    fixxed alu bug with slt. added more tests for alu

--- alu.v
+++ alu.v
@@ -3,7 +3,11 @@ module alu(	input[2:0] opcode_i,
 				input[7:0] rt_i,
 				output reg[7:0] alu_result_o,
 				output reg zero);
-								
+					
+		
+reg signed [7:0] rs_signed;
+reg signed [7:0] rt_signed;
+		
 		always@(*)begin
 		
 			case(opcode_i)
@@ -21,10 +25,13 @@ module alu(	input[2:0] opcode_i,
 					alu_result_o[7:0] = rs_i[7:0] >> 1;
 				end
 				3'b100: begin	//sub (signed) operation. $Rreg = $r2 - $r5
+					//rs_signed = rs_i;
+					//rt_signed = rt_i;
+					//alu_result_o[7:0] = rs_signed[7:0] - rt_signed[7:0];
 					alu_result_o[7:0] = rs_i[7:0] - rt_i[7:0];
 				end
 				3'b101: begin	//slt operation. if ($r6(rs_i) < $r7(rt_i)),  CB = 1 else CB = 0
-					if((rs_i - rt_i)> 16'd0)begin
+					if((rs_i - rt_i)>= 8'd0)begin
 						zero = 0;
 					end
 					else begin
@@ -32,7 +39,7 @@ module alu(	input[2:0] opcode_i,
 					end
 				end
 				3'b110: begin	//absolute operation. $Rreg = abs($Rreg)
-					if (rs_i < 16'd0) begin
+					if (rs_i[7] == 1'b1) begin
 						alu_result_o = -rs_i ;
 					end
 					else begin
--- alu_test.v
+++ alu_test.v
@@ -3,6 +3,7 @@ module alu_test;
 reg [7:0]rs_i,rt_i;
 reg [2:0] opcode_i;
 
+
 wire [7:0] alu_result_o;
 wire set_o;
 wire zero;
@@ -18,58 +19,87 @@ alu u0(
 initial 
 	begin
 
+	//test and
 	rs_i = 8'b01010101;
 	rt_i = 8'b10101010;
 	opcode_i = 3'b000;
 	#10
-		$display("Result is %d. Correct: 1", alu_result_o);
+		$display("Testing AND. Result is %d. Correct: 1", alu_result_o);
 	
-
+	//test and
+	rs_i = 8'b11111111;
+	rt_i = 8'b10101010;
+	opcode_i = 3'b000;
+	#10
+		$display("Testing AND. Result is %b. Correct: 10101010", alu_result_o);
+	
+	//test add
 	rs_i = 8'b00001111;
 	rt_i = 8'b11110000;
 	opcode_i = 3'b001;
 	#10
-		$display("Result is %d, Correct: 255", alu_result_o);
+		$display("Testing ADD. Result is %d, Correct: 255", alu_result_o);
 
-
+	//test add
+	rs_i = 8'd20;
+	rt_i = 8'd100;
+	opcode_i = 3'b001;
+	#10
+		$display("Testing ADD. Result is %d, Correct: 120",alu_result_o);
+		
+	//test sll
 	rs_i = 8'b00000101;
 	rt_i = 8'b00000010;
 	opcode_i = 3'b010;
 	#10
-		$display("Result is %d, Correct: 20", alu_result_o);
+		$display("Testing SLL. Result is %d, Correct: 20", alu_result_o);
 
+	//test sll
+	rs_i = 8'b00000101;
+	rt_i = 8'b11111111;
+	opcode_i = 3'b010;
+	#10
+		$display("Testing SLL. Result is %d, Correct: 0", alu_result_o);
 
+	//test srl
 	rs_i = 8'b11111110;
 	opcode_i = 3'b011;
 	#10
-		$display("Result is %d. Correct: 127", alu_result_o);
+		$display("Testing SRL. Result is %d. Correct: 127", alu_result_o);
 	
-
-	rs_i = 8'b11111101;
-	rt_i = 8'b11111010;
+	//test sub
+	rs_i = 8'b11111101; //253
+	rt_i = 8'b11111010; //250
 	opcode_i = 3'b100;
 	#10
-		$display("Result is %d. Correct: 3", alu_result_o);
+		$display("Testing SUB. Result is %d. Correct: 3", alu_result_o);
 	
-
+	//test sub
+	rs_i = 8'b11101100; //-20
+	rt_i = 8'b00000100; //4
+	opcode_i = 3'b100;
+	#10
+		$display("Testing SUB. Result is %d. Correct: -24", $signed(alu_result_o));
+		
+	//test set less than
 	rs_i = 8'b00000001;
 	rt_i = 8'b00000001;
 	opcode_i = 3'b101;
 	#10
-		$display("Result is %d. Correct: 1", zero);
+		$display("Testing SLL. Result is %d. Correct: 0", zero);
 	
-
+	//test abosolute
 	rs_i = 8'b11111111;
 	opcode_i = 3'b110;
 	#10
-		$display("Result is %d. Correct: 255", alu_result_o);
+		$display("Testing ABSOLUTE.Result is %d. Correct: 1", alu_result_o);
 	
-
+	//test set equals.
 	rs_i = 8'b11111111;
 	rt_i = 8'b11111111;
 	opcode_i = 3'b111;
 	#10
-		$display("Result is %d. Correct: 1", zero);
+		$display("Testing set equals. Result is %d. Correct: 1", zero);
 
 	end
 endmodule
