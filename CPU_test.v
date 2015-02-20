3db822e28de1a21cb281064ef18c58ca53a8e8b2
zizhouzhai <zizhou.zhai@hotmail.co.nz>
2015-02-19 19:31:26 -0800

    running on modelsim

--- CPU_test.v
+++ CPU_test.v
@@ -9,26 +9,30 @@ reg [7:0] start_addr;
 lab3 test_lab3(
 .start_i	(start_i),
 .start_addr	(start_addr),
-.clock_i	(clock_i)	
+.clock_i	(clock_i),
+.done (done)
 );
 
 always
 begin
 	#10
-	clk_i = 1'b1;
+	clock_i = 1'b1;
 	#10
-	clk_i = 1'b0;
+	clock_i = 1'b0;
 end
 
 initial
 begin
+
+	$display("starting first");
 	start_addr = 8'd0;
 	start_i = 1;
 	#5
 	start_i = 0;
 	wait(done == 1);
 	
-
+	$display("finished first");
+	
 	start_addr = 8'd93;
 	start_i = 1;
 	#5
